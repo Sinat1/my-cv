c7e8d6553a228da9c1552d8b17c7859284f55aae
Sinat1 <67416969+Sinat1@users.noreply.github.com>
2025-05-07 17:07:04 +0000

    Deploying to gh-pages from @ Sinat1/my-cv@b06d7780f46c557da7ad7beef8bc20955e061998 🚀

---
+++ index.78c39c39.js
@@ -0,0 +1,2 @@
+const o=document.getElementById("scrollToTopBtn");window.addEventListener("scroll",(()=>{window.pageYOffset>300?o.classList.add("show"):o.classList.remove("show")})),o.addEventListener("click",(()=>{window.scrollTo({top:0,behavior:"smooth"})}));
+//# sourceMappingURL=index.78c39c39.js.map
---
+++ index.e7113018.js
@@ -0,0 +1,2 @@
+!function(){var o=document.getElementById("scrollToTopBtn");window.addEventListener("scroll",(function(){window.pageYOffset>300?o.classList.add("show"):o.classList.remove("show")})),o.addEventListener("click",(function(){window.scrollTo({top:0,behavior:"smooth"})}))}();
+//# sourceMappingURL=index.e7113018.js.map
